1f46d84ce854a433d6eb0ad55e6a2c572b307c60
Geoffrey Michael Rendon <geoffrey.m.rendon@gmail.com>
2023-12-12 14:35:56 -0600

    variable Lecture

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
-    System.out.println("Hello, World!");
+        System.out.println("This is an example of a statement");
+
+        /*
+        * This is a Comment example!
+        *
+        */
+
+        byte smallNum = 19;
+        System.out.println(smallNum);
+        smallNum = 127;
+        int notAsSmallNum = smallNum;
+        smallNum ++;
+        notAsSmallNum ++;
+        System.out.println(smallNum);
+        System.out.println(notAsSmallNum);
+
+        int numDefault;
+        /*  numDefault = 0; -- you MUST set a value for a variable.
+        *   In other words there is no default value.
+        System.out.println(numDefault); */
+
+        char firstLetterOfGeoff = 'G';
+
+        String message = "I'm a string!";
+        String quote = "\"I'm a better String!.\" - Word ";
+        System.out.println(message);
+        System.out.println(quote);
+
+        final String EMPEROR_OF_CONFUSION = "Rick Sanchez";
+        System.out.println(EMPEROR_OF_CONFUSION);
     }
 }
